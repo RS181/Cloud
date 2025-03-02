66491c360372ffe3bec1fc8cfdbce213c9aa25f7
RS181 <ruisantos.18181@gmail.com>
2025-03-02 10:15:32 +0000

    Conexao a b.d do Servidor

--- Rest_API_Servidor/jax-rs/jersey/jersey-jetty/src/main/java/com/mkyong/MainApp.java
+++ Rest_API_Servidor/jax-rs/jersey/jersey-jetty/src/main/java/com/mkyong/MainApp.java
@@ -5,12 +5,14 @@ import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.net.URI;
+import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainApp {
 
     public static final String BASE_URI = "http://localhost:4040/";
+    public static final Connection teste = DB.OpenDatabase();
 
     public static Server startServer() {
 
@@ -20,6 +22,7 @@ public class MainApp {
         final ResourceConfig config = new ResourceConfig(MyResource.class,Upload.class,Download.class);
         final Server server =
                 JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
+
 
         return server;
 
@@ -35,6 +38,8 @@ public class MainApp {
                 try {
                     System.out.println("Shutting down the application...");
                     server.stop();
+                    System.out.println("Closing connection to Database");
+                    DB.CloseDatabase();
                     System.out.println("Done, exit.");
                 } catch (Exception e) {
                     Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, e);
@@ -45,10 +50,6 @@ public class MainApp {
 
             // block and wait shut down signal, like CTRL+C
             Thread.currentThread().join();
-
-            // alternative
-            // Thread.sleep(Long.MAX_VALUE);       // sleep forever...
-            // Thread.sleep(Integer.MAX_VALUE);    // sleep around 60+ years
 
         } catch (InterruptedException ex) {
             Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
