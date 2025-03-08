2ec5c15952b4dea80e55e6783ddc05fef0c5e532
RS181 <ruisantos.18181@gmail.com>
2025-03-08 09:41:28 +0000

    Classe com caminhos absolutos

---
+++ Rest_API_Replica/jax-rs/jersey/jersey-jetty/src/main/java/com/mkyong/AbsolutePath.java
@@ -0,0 +1,15 @@
+package com.mkyong;
+
+/**
+ * Class that contains absolute path used in Replica Api
+ */
+public class AbsolutePath {
+    // Location where we store Uploaded Resource's
+    private static final String STORAGE_FOLDER =  "/home/rui/Desktop/Replica_File_Storage/";
+    
+    // URL to test API of Main Server
+    private static final String SERVER_TEST_API_URL = "http://localhost:4040/hello";
+
+    // URL of Upload API of Mian server 
+    private static final String SERVER_UPLOAD_API_URL = "http://localhost:4040/upload-main/resource";    
+}
---
+++ Rest_API_Servidor/jax-rs/jersey/jersey-jetty/src/main/java/com/mkyong/AbsolutePath.java
@@ -0,0 +1,9 @@
+package com.mkyong;
+/**
+ * Class that contains absolute path used in Server Api
+ */
+public class AbsolutePath {
+    // Location where we store Uploaded Resource's
+    private static final String STORAGE_FOLDER =  "/home/rui/Desktop/Servidor_File_Storage/";
+
+}
